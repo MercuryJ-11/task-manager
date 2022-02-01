5c022ba3f62aa2a13a85adc2c668ba2b6569e051
Jin DU <telepathymira@gmail.com>
2022-02-01 12:14:52 +0100

    Add columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_110305) do
+ActiveRecord::Schema.define(version: 2022_02_01_110508) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
